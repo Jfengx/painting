011e88e17d50bdaef70b280a64bcfb98b11f7bf5
JiaFeng <18800295187@163.com>
2022-05-16 23:22:33 +0800

    fix: routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
-import Gallery from '../views/Gallery.vue';
 
 const routes = [
   { path: '/', component: Home, meta: { title: 'Home' } },
-  { path: '/gallery', component: () => Gallery, meta: { title: 'Gallery' } },
+  { path: '/gallery', component: () => import('../views/Gallery.vue'), meta: { title: 'Gallery' } },
   { path: '/world', component: () => import('../views/World.vue'), meta: { title: 'World' } },
 ];
 
